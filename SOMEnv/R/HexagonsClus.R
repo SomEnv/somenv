116a2e8e90dba1f3d1897078f6dabb3bd3706d19
Tommaso Rodani <trodani@sissa.it>
2020-10-23 17:07:25 +0200

    fix typo

--- SOMEnv/R/HexagonsClus.R
+++ SOMEnv/R/HexagonsClus.R
@@ -12,6 +12,7 @@
 #' @author Sabina Licen
 #' @return A SOM map colored according to cluster splitting
 #' @importFrom grDevices rainbow
+#' @importFrom graphics text
 #' @references {Licen, S., Cozzutto, S., Barbieri, P. (2020) Aerosol Air Qual. Res., 20 (4), pp. 800-809. DOI: 10.4209/aaqr.2019.08.0414
 #' }
 #' @export
